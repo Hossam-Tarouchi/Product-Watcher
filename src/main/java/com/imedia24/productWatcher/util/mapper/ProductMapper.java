c59d0813d9d85b7eacdbab4a72ab475385b60da5
Hossam <>
2024-11-16 17:19:50 +0100

    Add product mappers

---
+++ src/main/java/com/imedia24/productWatcher/util/mapper/ProductMapper.java
@@ -0,0 +1,28 @@
+package com.imedia24.productWatcher.util.mapper;
+
+import com.imedia24.productWatcher.dao.entity.ProductEntity;
+import com.imedia24.productWatcher.dao.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public ProductEntity toProductEntity(Product product) {
+        if (product.getSku() == null) {
+            return ProductEntity.builder()
+                    .name(product.getName())
+                    .build();
+        }
+        return ProductEntity.builder()
+                .sku(product.getSku())
+                .name(product.getName())
+                .build();
+    }
+
+    public Product toProduct(ProductEntity productEntity) {
+        return Product.builder()
+                .sku(productEntity.getSku())
+                .name(productEntity.getName())
+                .build();
+    }
+}
