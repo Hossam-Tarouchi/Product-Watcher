e9c2acc8bdd63c2997ed120a666f9622a53ae12b
Hossam <>
2024-11-16 15:09:44 +0100

    Add model pojo

---
+++ src/main/java/com/imedia24/productWatcher/dao/model/Product.java
@@ -0,0 +1,15 @@
+package com.imedia24.productWatcher.dao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    private Long sku;
+    private String name;
+}
